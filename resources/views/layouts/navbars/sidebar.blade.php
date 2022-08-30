d80b9a9f01e6a7556156069c3ff250684f3fb1e8
askar <mr.askar1984@gmail.com>
2022-08-30 12:35:50 +0200

    b

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -33,10 +33,10 @@
                 <span class="sidebar-normal">{{ __('إضافة متغير') }} </span>
               </a>
             </li>
-            <!--<li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
-              <a class="nav-link" href="{{ route('search.index') }}">
+            <li class="nav-item{{ $activePage == 'upload' ? ' active' : '' }}">
+              <a class="nav-link" href="{{ url('/multiuploads') }}">
                 <span class="sidebar-mini"> UM </span>
-                <span class="sidebar-normal"> {{ __('البحث عن متغير') }} </span>-->
+                <span class="sidebar-normal"> {{ __('المرفقات') }} </span>
               </a>
             </li>
           </ul>
