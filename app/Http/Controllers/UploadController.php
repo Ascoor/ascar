b4365a07f49858a119befbcc47f33319ddb16551
askar <mr.askar1984@gmail.com>
2022-08-30 12:39:14 +0200

    gggg

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Http\Controllers;
+use App\Item;
+use App\ItemDetail;
+use Illuminate\Http\Request;
+class UploadController extends Controller
+{
+public function uploadForm()
+{
+return view('place.upload_form');
+}
+public function uploadSubmit(Request $request)
+{
+$this->validate($request, [
+
+ 
+ 'gnum1'=>'required',
+ 'gnum2'=>'required',
+ 'gnum3'=>'required',
+ 'gnum4'=>'required',
+
+]);
+if($request->hasFile('photos'))
+{
+$allowedfileExtension=['pdf','jpg','png','docx'];
+$files = $request->file('photos');
+foreach($files as $file){
+$filename = $file->getClientOriginalName();
+$extension = $file->getClientOriginalExtension();
+$check=in_array($extension,$allowedfileExtension);
+//dd($check);
+if($check)
+{
+$items= Item::create($request->all());
+foreach ($request->photos as $photo) {
+$filename = $photo->store('photos');
+ItemDetail::create([
+'item_id' => $items->id,
+'filename1' => $filename
+]);
+}
+echo "Upload Successfully";
+}
+else
+{
+echo '<div class="alert alert-warning"><strong>Warning!</strong> Sorry Only Upload png , jpg , doc</div>';
+}
+}
+}
+}
+}?>
