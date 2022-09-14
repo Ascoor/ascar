54d78e9ef418da4d0c94376b53e75cd0203916bc
askar <mr.askar1984@gmail.com>
2022-09-14 09:03:04 +0200

    logo

--- resources/views/layouts/page_templates/guest.blade.php
+++ resources/views/layouts/page_templates/guest.blade.php
@@ -1,20 +1,23 @@
 @include('layouts.navbars.navs.guest')
 <div class="wrapper wrapper-full-page">
-  <div class="page-header login-page header-filter" filter-color="black" style="background-image: url('{{ asset('material') }}/img/login.jpg'); background-size: cover; background-position: top center;align-items: center;" data-color="purple">
-  <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
-  <div class="container" style="height: 1250px;
+    <div class="page-header login-page header-filter" filter-color="black"
+        style="background-image: url('{{ asset('material') }}/img/login.jpg'); background-size: cover; background-position: top center;align-items: center;"
+        data-color="purple">
+        <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
+        <div class="container" style="height: 1250px;
   width:1650;
   ">
-   <div class="logo">
-       <img src="/img/coat.png" class="rounded mx-auto d-block" alt="...">
+            <div class="logo">
+                <img src="/img/Coat.png" class="rounded mx-auto d-block" alt="...">
+            </div>
+            <h1 class="text text-center" style="color: rgb(231, 244, 3);font-size: 42pt;">{{ __('محافظة الدقهلية') }}
+            </h1></a>
+            <h2 class="text-white text-center">{{ __('نظام إدارة المتغيرات المكانية') }}</h2>
+
+
+            @yield('content')
+
+        </div>
+        @include('layouts.footers.guest')
     </div>
-    <h1 class="text text-center" style="color: rgb(231, 244, 3);font-size: 42pt;">{{ __('محافظة الدقهلية') }}</h1></a>
-    <h2 class="text-white text-center">{{ __('نظام إدارة المتغيرات المكانية') }}</h2>
-
-
-@yield('content')
-
-</div>
-    @include('layouts.footers.guest')
-  </div>
 </div>
