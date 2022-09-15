14a574aa755aae09464fce58a28f41cccd209d6d
askar <mr.askar1984@gmail.com>
2022-09-15 19:48:18 +0200

    sy

--- resources/views/layouts/footers/auth.blade.php
+++ resources/views/layouts/footers/auth.blade.php
@@ -1,15 +1,15 @@
 <footer class="footer">
-    <center>
 
 
-        <div class="copyright float">
+    <div class="copyright float">
+            <center>
             &copy;
             <script>
             document.write(new Date().getFullYear())
             </script> made with Ask-ar T.S<i class="material-icons"></i>
             <a href="https://www.creative-tim.com" target="_blank"></a> جميع الحقوق محفوظة لمحافظة الدقهلية <a
-                href="https://www.ask-ar.com" target="_blank"></a>تصميم وبرمجة المهندس أحمد محمد عسكر
+            href="https://www.ask-ar.com" target="_blank"></a>تصميم وبرمجة المهندس أحمد محمد عسكر
+        </center>
         </div>
-    </center>
     </div>
 </footer>
