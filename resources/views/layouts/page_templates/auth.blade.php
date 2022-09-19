72eeb88316b410ff6fabb836080b54d1e058cc64
askar <mr.askar1984@gmail.com>
2022-09-19 14:10:21 +0200

    up

--- resources/views/layouts/page_templates/auth.blade.php
+++ resources/views/layouts/page_templates/auth.blade.php
@@ -26,26 +26,10 @@ s
                     فهرس المتغيرات
                 </button>
                 <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                    <a class="dropdown-item" href="#">Dropdown link</a>
 
                     <a class="dropdown-item" href="{{ route('places.create') }}">{{ __('إضافة متغير') }}</a>
                     <a class="dropdown-item" href="{{ route('places.index') }}">{{ __('المتغيرات قيد التشغيل') }}</a>
                     <a class="dropdown-item" href="{{ route('place.trash') }}">{{ __('المتغيرات قيد الحفظ') }}</a>
-                </div>
-            </div>
-            <div class="btn-group" role="group" style="align-items: center">
-                <a class="btn btn-primary btn-lg"" type=" button" href="{{ route('home') }}">الرئيسية
-                </a>
-            </div>
-            <div class="btn-group" role="group">
-                <button id="btnGroupDrop1" type="button" class="btn btn-success btn-lg" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
-                    فهرس المتغيرات
-                </button>
-                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                    <a class="dropdown-item" href="#">Dropdown link</a>
-                    <a class="dropdown-item" href="{{ route('tags') }}">{{ __('قائمة الحالات') }}</a>
-                    <a class="dropdown-item" href="{{ route('tag.create') }}">{{ __('إضافة حالة') }}</a>
                 </div>
             </div>
 
@@ -55,28 +39,31 @@ s
                     إدارة الحالة
                 </button>
                 <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+
+                    <a class="dropdown-item" href="{{ route('tags') }}">{{ __('قائمة الحالات') }}</a>
+                    <a class="dropdown-item" href="{{ route('tag.create') }}">{{ __('إضافة حالة') }}</a>
                 </div>
             </div>
-        </div>
 
 
 
 
-        <div class="card">
-            <div class="card-body">
-                <div class="card-header card-header" style="
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-header card-header" style="
             color: rgb(255, 242, 64);
             background-color: #d3e6ff;
             align-items: center;
             text-align: center;">
-                    <h1 class=" display-4">
-                        <img class="card-img-top" style="width:350px;
+                        <h1 class=" display-4">
+                            <img class="card-img-top" style="width:350px;
                 height:350px ;
                 padding:auto ;   " src="/img/main.png">
+                    </div>
+
+                    @yield('content')
+
+                    @include('layouts.footers.auth')
                 </div>
-
-                @yield('content')
-
-                @include('layouts.footers.auth')
             </div>
-        </div>
