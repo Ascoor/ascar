3a72732886791a53fa777ae1e9db2f6089df7f67
askar <mr.askar1984@gmail.com>
2022-10-15 08:17:41 +0200

    Revert "Revert "ok""
    This reverts commit 5647ffe187de0b9a372d4e365e60d271f1ae9e59.

---
+++ resources/views/place/export.blade..php
@@ -0,0 +1,48 @@
+@extends('layouts.app', ['activePage' => 'search', 'titlePage' => __('بحث')])
+
+@section('content')
+
+<div class="card">
+    <div class="card-header card-header" style="
+    color: rgb(255, 242, 64);
+    background-color: #153257;
+    align-items: center;
+    text-align: center;">
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Email</th>
+                    <th>Email</th>
+                    <th>Email</th>
+                    <th>Email</th>
+                    <th>Email</th>
+                    <th>Email</th>
+                    <th>Email</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($places as $place)
+                <tr>
+                    <td>{{ $place->gnump1 }}</td>
+                    <td>{{ $place->gnump2 }}</td>
+                    <td>{{ $place->gnump3 }}</td>
+                    <td>{{ $place->gnump4 }}</td>
+                    <td>{{ $place->gnump5 }}</td>
+                    <td>{{ $place->gnump6 }}</td>
+                    <td>{{ $place->gnump7 }}</td>
+                    <td>{{ $place->gnump8 }}</td>
+                    <td>{{ $place->gnump9 }}</td>
+                    <td>{{ $place->gnump11 }}</td>
+                    <td>{{ $place->gnump13 }}</td>
+
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+
+        @endsection
