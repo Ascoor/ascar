05b82d78a45a6a6be3091cacddc7f6d27983d7eb
askar <mr.askar1984@gmail.com>
2022-10-15 08:38:29 +0200

    Revert "Revert "datatable""
    This reverts commit 861aaa6fc4decdb34a13555f60f4868dd88b10f2.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,8 @@
 
     <!-- CSS -->
 
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/v/bs4/dt-1.12.1/b-2.2.3/sc-2.0.7/sb-1.3.4/sl-1.4.0/datatables.min.css" />
 
     <link rel="stylesheet" type="text/css"
         href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/fonts/simple-line-icons/style.min.css">
@@ -105,9 +107,12 @@
             integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
         </script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-       
-     integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
         </script>
+        <script type="text/javascript"
+            src="https://cdn.datatables.net/v/bs4/dt-1.12.1/b-2.2.3/sc-2.0.7/sb-1.3.4/sl-1.4.0/datatables.min.js">
+        </script>
+
         <script src="https://cdn.rtlcss.com/bootstrap/v4.5.3/js/bootstrap.min.js"
             integrity="sha384-VmD+lKnI0Y4FPvr6hvZRw6xvdt/QZoNHQ4h5k0RL30aGkR9ylHU56BzrE2UoohWK" crossorigin="anonymous">
         </script>
