e4c8bc66abd2eea4ab6deccea38d5c70231d203a
askar <mr.askar1984@gmail.com>
2022-10-20 00:47:02 +0200

    god unteil edit

--- resources/views/place/create.blade.php
+++ resources/views/place/create.blade.php
@@ -3,8 +3,6 @@
 
 @section('content')
 
-
-
 <div class="card">
     <div class="card-header card-header" style="
     color: rgb(255, 242, 64);
@@ -20,119 +18,135 @@
     </div>
 
 </div>
-<div class="col">
-    <div class="card">
-        <div class="card-body">
 
-            @if($message = Session::get('تمت'))
-            <div class="alert alert-danger" role="alert">
-                {{ $message }}
-            </div>
-            @endif
-            @if(session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
+<div class="container">
+
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+
+                @if($message = Session::get('تمت'))
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
+                </div>
                 @endif
+                @if(session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                    @endif
 
 
-                @if(count($errors) > 0)
-                <ul>
-                    @foreach($errors->all() as $item)
-                    <li>
-                        {{ $item }}
-                    </li>
-                    @endforeach
-                </ul>
-                @endif
-                <br>
-
-                <form action="{{route('place.store')}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">مسلسل المتغير </label>
-                        <input type="number" name="gnump" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        @foreach($tags as $item)
-                        <input type="checkbox" name="tags[]" value="{{ $item->id }}">
-                        <label for="">{{ $item->tag }}</label>
+                    @if(count($errors) > 0)
+                    <ul>
+                        @foreach($errors->all() as $item)
+                        <li>
+                            {{ $item }}
+                        </li>
                         @endforeach
+                    </ul>
+                    @endif
+                    <br>
 
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">خط العرض </label>
-                        <input type="float" name="gnumh" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">خط الطول </label>
-                        <input type="float" name="gnumw" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">المحافظة </label>
-                        <input type="text" name="gnump1" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1"> المركز </label>
-                        <input type="text" name="gnump2" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">القرية </label>
-                        <input type="text" name="gnump3" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">نوع الأملاك </label>
-                        <input type="text" name="gnump4" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">جهة الولاية </label>
-                        <input type="text" name="gnump5" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">القانونية </label>
-                        <input type="text" name="gnump6" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">الرد </label>
-                        <input type="text" name="gnump7" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">تفاصيل الرد </label>
-                        <input type="text" name="gnump8" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">موقف الإزالة </label>
-                        <input type="text" name="gnump9" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1"> التاريخ </label>
-                        <input type="date" name="gnump10" class="form-control">
-                    </div>
+                    <form action="{{route('place.store')}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
 
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">الملاحظات </label>
-                        <textarea class="form-control" name="gnump11" rows="3"></textarea>
+                            <label for="exampleFormControlInput1">الإجراءات المنتهية </label>
+
+                            @foreach($tags as $item)
+                            <input type="checkbox" name="tags[]" value="{{ $item->id }}">
+                            <label for="">{{ $item->tag }}</label>
+                            @endforeach
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">وضع المتغير </label>
+
+                            <select class="form-control" name="category_id" id="category">
+                                @foreach ($categories as $category)
+                                <option value="{{$category->id}}">{{$category->name}}</option>
+
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">مسلسل المتغير </label>
+                            <input type="number" name="gnump" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">خط العرض </label>
+                            <input type="float" name="gnumh" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">خط الطول </label>
+                            <input type="float" name="gnumw" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">المحافظة </label>
+                            <input type="text" name="gnump1" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1"> المركز </label>
+                            <input type="text" name="gnump2" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">القرية </label>
+                            <input type="text" name="gnump3" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">نوع الأملاك </label>
+                            <input type="text" name="gnump4" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">جهة الولاية </label>
+                            <input type="text" name="gnump5" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">القانونية </label>
+                            <input type="text" name="gnump6" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">الرد </label>
+                            <input type="text" name="gnump7" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">تفاصيل الرد </label>
+                            <input type="text" name="gnump8" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">موقف الإزالة </label>
+                            <input type="text" name="gnump9" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1"> التاريخ </label>
+                            <input type="date" name="gnump10" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">الملاحظات </label>
+                            <textarea class="form-control" name="gnump11" rows="3"></textarea>
 
 
-                    </div>
+                        </div>
 
-                    <div class="form-group">
+                        <div class="form-group">
 
-                        <label for="exampleFormControlInput1">الصور </label>
-                        <input type="file" name="photo1[]" multiple>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">المرفقات </label>
-                        <input type="file" name="photo2" class="form-control">
-                    </div>
+                            <label for="exampleFormControlInput1">الصور </label>
+                            <input type="file" name="photo1[]" multiple>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">المرفقات </label>
+                            <input type="file" name="photo2" class="form-control">
+                        </div>
 
-                    <div class="form-group">
+                        <div class="form-group">
 
-                        <button class="btn btn-danger" type="submit">save</button>
-                    </div>
+                            <button class="btn btn-danger" type="submit">save</button>
+                        </div>
 
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
 
-    @endsection
+        @endsection
